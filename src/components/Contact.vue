5a41f052e976221fcad9774884486c3a4130907d
Antoni Kucharczyk <hello@antoni.work>
2022-01-14 19:03:13 +0100

    Fixes

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -160,26 +160,7 @@
             </dl>
             <ul role="list" class="mt-8 flex space-x-12">
               <li>
-                <a class="text-gray-200 hover:text-indigo-100" href="#">
-                  <span class="sr-only">Facebook</span>
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="w-6 h-6"
-                    aria-hidden="true"
-                  >
-                    <path
-                      d="M22.258 1H2.242C1.556 1 1 1.556 1 2.242v20.016c0 .686.556 1.242 1.242 1.242h10.776v-8.713h-2.932V11.39h2.932V8.887c0-2.906 1.775-4.489 4.367-4.489 1.242 0 2.31.093 2.62.134v3.037l-1.797.001c-1.41 0-1.683.67-1.683 1.653v2.168h3.362l-.438 3.396h-2.924V23.5h5.733c.686 0 1.242-.556 1.242-1.242V2.242C23.5 1.556 22.944 1 22.258 1"
-                      fill="currentColor"
-                    />
-                  </svg>
-                </a>
-              </li>
-              <li>
-                <a class="text-gray-200 hover:text-indigo-100" href="#">
+                <a class="text-gray-200 hover:text-indigo-100" href="https://github.com/akucharczyk" target="_blank">
                   <span class="sr-only">GitHub</span>
                   <svg
                     width="24"
@@ -198,7 +179,7 @@
                 </a>
               </li>
               <li>
-                <a class="text-gray-200 hover:text-indigo-100" href="#">
+                <a class="text-gray-200 hover:text-indigo-100" href="https://twitter.com/antoni_ku" target="_blank">
                   <span class="sr-only">Twitter</span>
                   <svg
                     width="24"
@@ -327,7 +308,7 @@
               <div class="sm:col-span-2 sm:flex sm:justify-end">
                 <button
                   type="submit"
-                  class="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium text-white rounded-md shadow-md shadow-purple-500/70 hover:shadow-purple-500/40 ease-in-out duration-150 bg-gradient-to-br from-pink-600 to-purple-600 hover:bg-gradient-to-r transition-all"
+                  class="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium text-white rounded-md shadow-md shadow-purple-500/70 hover:shadow-purple-500/40 ease-in-out duration-150 bg-gradient-to-br from-pink-600 to-purple-600 hover:bg-gradient-to-br transition-all"
                 >Submit</button>
               </div>
             </form>
